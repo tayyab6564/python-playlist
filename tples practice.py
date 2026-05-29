448d42400fa215fbaa25174ea3d4f95975b2be89
tayyab6564 <asadali6564265642@gmail.com>
2026-05-28 22:17:19 -0700

    q

---
+++ tples practice.py
@@ -0,0 +1,8 @@
+grades = ["A", "B", "C","A", "A", "B"] #creating a tuple of grades  
+
+#counting how many times "A" appears in the tuple, output: 3 
+ 
+print(grades.count("A"))
+
+# sorting the tuple, output: ('A', 'A', 'A', 'B', 'B', 'C')
+print(sorted(grades))
